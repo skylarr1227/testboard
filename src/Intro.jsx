470e15b3d4a3833fdf0e6ce041d085a443495efe
Sky <44381751+skylarr1227@users.noreply.github.com>
2023-11-06 16:22:53 -0500

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,15 +3,10 @@ import './Intro.css';
 function Intro() {
   return (
     <div className="intro">
-      <h1>D3.js Leaderboard</h1>
+      <h1>DITTOBOT Leaderboard Data</h1>
 
       <p>
-        Here is a small leaderboard made with D3.js. It is responsive, updates in real-time and can be easily customized.
-      </p>
-
-      <p>
-        Check out the <a href="https://github.com/rhidra/d3js-leaderboard" target="_blank">source code here</a>, and 
-        the original <a href="https://blog.remyhidra.dev/" target="_blank">blog post here</a>.
+        Bugs may be present, work in progress.
       </p>
     </div>
   );
